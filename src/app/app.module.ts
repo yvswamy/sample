ed3cff4749469f63398ea2c3174dd36525fd5118
Veeraswamy Yarramsetty <vyarrams@in.ibm.com>
2018-11-22 08:53:26 +0530

    Angular 7 Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+
+import { carbonCoponents } from './app.routing';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    ...carbonCoponents,
+    FormsModule,
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,33 @@
+import { CarbonButtonModule,
+  CarbonDropdownModule,
+  CarbonCopyButtonModule,
+  CarbonCheckboxModule,
+  CarbonAccordionModule,
+  CarbonPaginationModule,
+  CarbonBasicNotificationModule,
+  CarbonIconModule,
+  CarbonModalModule,
+  CarbonOverflowModule,
+  CarbonCardModule,
+
+  // CarbonBreadcrumbComponent,1
+  // CarbonBreadcrumbItemComponent
+
+} from '@cccc/angular-carbon-components';
+
+export const carbonCoponents = [
+  CarbonButtonModule,
+  CarbonDropdownModule,
+  CarbonCopyButtonModule,
+  CarbonCheckboxModule,
+  CarbonAccordionModule,
+  CarbonPaginationModule,
+  CarbonBasicNotificationModule,
+  CarbonIconModule,
+  CarbonModalModule,
+  CarbonOverflowModule,
+  CarbonCardModule,
+  // CarbonBreadcrumbComponent,
+  // CarbonBreadcrumbItemComponent,
+
+];
